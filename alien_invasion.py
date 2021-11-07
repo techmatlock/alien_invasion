c9f48bcf4b8a4787382ee61986cde07240914610
Mark <techmatlock@gmail.com>
2021-11-07 12:43:07 -0600

    Added events if right key is pressed or released.

--- alien_invasion.py
+++ alien_invasion.py
@@ -30,8 +30,10 @@ class AlienInvasion:
                     sys.exit()
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
-                    # Move the ship to the right.
-                    self.ship.rect.x += 1
+                    self.ship.moving_right = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
