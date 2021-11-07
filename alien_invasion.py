4d40062156fe66cfe0f7035f613d262ba01c8ff2
Mark <techmatlock@gmail.com>
2021-11-07 12:45:10 -0600

    Added ship update method.

--- alien_invasion.py
+++ alien_invasion.py
@@ -21,6 +21,7 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
   
     def _check_events(self):
