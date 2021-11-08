1b8630c2c0416b11f9e749bd24e37045a6f3e906
Mark Matlock <techmatlock@gmail.com>
2021-11-08 16:06:45 -0600

    Limit number of bullets.

--- settings.py
+++ settings.py
@@ -13,6 +13,7 @@ class Settings:
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
 
         # Ship settings
         self.ship_speed = 1.5
