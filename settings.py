151ab2a7116a56c4a5ddfca3ebe385df28681a80
Mark <techmatlock@gmail.com>
2021-11-09 17:51:38 -0600

    Add setting to control speed of each alien.

--- settings.py
+++ settings.py
@@ -7,6 +7,9 @@ class Settings:
         self.screen_width = 1200 
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
+
+        # Alien settings
+        self.alien_speed = 1.0
         
         # Bullet settings
         self.bullet_speed = 1.0
