e39f63b56eca45f5cff734b221f9b45c5a3e6d7b
Mark <techmatlock@gmail.com>
2021-11-09 17:53:22 -0600

    Create settings parameter in __init__.

--- alien.py
+++ alien.py
@@ -8,6 +8,7 @@ class Alien(Sprite):
         """Initialize the alien and set its starting position."""
         super().__init__()
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
 
         # Load the alien image and set its rect attribute.
         self.image = pygame.image.load('images/alien.bmp')
