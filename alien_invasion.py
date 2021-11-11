7bb98f8de7a35d3260e7138710832e8440de4b92
Mark Matlock <techmatlock@gmail.com>
2021-11-11 14:41:15 -0600

    Draw the play button.

--- alien_invasion.py
+++ alien_invasion.py
@@ -191,6 +191,10 @@ class AlienInvasion:
             bullet.draw_bullet()
         self.aliens.draw(self.screen)
 
+        # Draw the play button if the game is inactive.
+        if not self.stats.game_active:
+            self.play_button.draw_button()
+
         pygame.display.flip()
 
 if __name__ == '__main__':
