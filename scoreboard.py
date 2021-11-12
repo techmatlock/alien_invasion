596e31a434322e85f776c4bb74932a8c142c4a0f
Mark Matlock <techmatlock@gmail.com>
2021-11-12 14:38:52 -0600

    Draw high score top center of screen.

--- scoreboard.py
+++ scoreboard.py
@@ -44,3 +44,4 @@ class Scoreboard:
     def show_score(self):
         """Draw score to the screen."""
         self.screen.blit(self.score_image, self.score_rect)
+        self.screen.blit(self.high_score_image, self.high_score_rect)
