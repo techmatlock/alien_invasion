6f374587afb14e23b0fd5316b191a720e6257f6c
Mark Matlock <techmatlock@gmail.com>
2021-11-12 14:40:35 -0600

    Check if new high score.

--- scoreboard.py
+++ scoreboard.py
@@ -45,3 +45,9 @@ class Scoreboard:
         """Draw score to the screen."""
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
+
+    def check_high_score(self):
+        """Check to see if there's a new high score."""
+        if self.stats.score > self.stats.high_score:
+            self.stats.high_score = self.stats.score
+            self.prep_high_score()
