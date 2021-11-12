1ed10b410867807145645ea4c665f9a8c0c513e9
Mark <techmatlock@gmail.com>
2021-11-12 16:28:25 -0600

    Added prep_level in _start_game().

--- alien_invasion.py
+++ alien_invasion.py
@@ -113,6 +113,7 @@ class AlienInvasion:
         self.stats.reset_stats()
         self.stats.game_active = True
         self.sb.prep_score()
+        self.sb.prep_level()
 
         # Hide the mouse cursor.
         pygame.mouse.set_visible(False)
