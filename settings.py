f7f361db523f82691475b7fb7e4ea2c1332d920c
Mark Matlock <techmatlock@gmail.com>
2021-11-12 09:51:28 -0600

    Increase speed settings.

--- settings.py
+++ settings.py
@@ -27,7 +27,7 @@ class Settings:
 
         # How quickly the game speeds up
         self.speedup_scale = 1.1
-        
+
         self.initialize_dynamic_settings()
 
     def initialize_dynamic_settings(self):
@@ -37,3 +37,9 @@ class Settings:
         self.alien_speed = 1.0
         # fleet_direction of 1 represents right; -1 represents left
         self.fleet_direction = 1
+
+    def increase_speed(self):
+        """Increase speed settings."""
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
