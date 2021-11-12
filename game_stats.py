e4b5526102bf473f9655960b097c3841c699e6cd
Mark Matlock <techmatlock@gmail.com>
2021-11-12 14:30:38 -0600

    Initialize high_score.

--- game_stats.py
+++ game_stats.py
@@ -6,6 +6,9 @@ class GameStats:
         self.settings = ai_game.settings
         self.reset_stats()
 
+        # High score should never be reset.
+        self.high_score = 0
+
         # Start game in an inactive state.
         self.game_active = False
 
