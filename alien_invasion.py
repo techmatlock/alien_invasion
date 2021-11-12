9a10da5abc9aa50c1f9d3edd9644ad9c9137f1d1
Mark Matlock <techmatlock@gmail.com>
2021-11-12 14:01:17 -0600

    Make sure to score all hits.

--- alien_invasion.py
+++ alien_invasion.py
@@ -170,7 +170,8 @@ class AlienInvasion:
             self.bullets, self.aliens, True, True)
 
         if collisions:
-            self.stats.score += self.settings.alien_points
+            for aliens in collisions.value():
+                self.stats.score += self.settings.alien_points * len(aliens)
             self.sb.prep_score()
         
         if not self.aliens:
