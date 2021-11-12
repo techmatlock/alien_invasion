7c15c7c324c8579d6e18f97abee9480c37988613
Mark <techmatlock@gmail.com>
2021-11-12 16:41:08 -0600

    Show how many ships are left.

--- scoreboard.py
+++ scoreboard.py
@@ -47,6 +47,15 @@ class Scoreboard:
         self.level_rect.right = self.score_rect.right
         self.level_rect.top = self.score_rect.bottom + 10
 
+    def prep_ships(self):
+        """Show how many ships are left."""
+        self.ships = Group()
+        for ship_number in range(self.stats.ships_left):
+            ship = Ship(self.ai_game)
+            ship.rect.x = 10 + ship_number * ship.rect.width
+            ship.rect.y = 10
+            self.ships.add(ship)
+
     def prep_high_score(self):
         """Turn the high score into a rendered image."""
         high_score = round(self.stats.high_score, -1)
