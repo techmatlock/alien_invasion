95966afcd73f42d23101fa4d993a0d5ce589b2a4
Mark Matlock <techmatlock@gmail.com>
2021-11-12 10:07:45 -0600

    Added space between code.

--- settings.py
+++ settings.py
@@ -35,6 +35,7 @@ class Settings:
         self.ship_speed = 1.5
         self.bullet_speed = 3.0
         self.alien_speed = 1.0
+        
         # fleet_direction of 1 represents right; -1 represents left
         self.fleet_direction = 1
 
