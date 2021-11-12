3217b4b230d51d2d7d479d8b738dd1c4af7f0f12
Mark Matlock <techmatlock@gmail.com>
2021-11-12 12:02:21 -0600

    Moved code to initalize_dynamic_ settings method.

--- settings.py
+++ settings.py
@@ -9,13 +9,9 @@ class Settings:
         self.bg_color = (230, 230, 230)
 
         # Alien settings
-        self.alien_speed = 0.2
         self.fleet_drop_speed = 10
-        # fleet_direction of 1 represents right; -1 represents left
-        self.fleet_direction = 1
         
         # Bullet settings
-        self.bullet_speed = 1.5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
@@ -23,19 +19,35 @@ class Settings:
 
         # Ship settings
         self.ship_speed = 1.5
-        self.ship_limit = 3
 
         # How quickly the game speeds up
         self.speedup_scale = 1.1
+
+        self.difficulty_level = 'medium'
 
         self.initialize_dynamic_settings()
 
     def initialize_dynamic_settings(self):
         """Initialize settings that change throughout the game."""
-        self.ship_speed = 1.5
-        self.bullet_speed = 3.0
-        self.alien_speed = 1.0
-        
+        if self.difficulty_level == 'easy':
+            self.ship_limit = 5
+            self.bullets_allowed = 10
+            self.ship_speed = 0.75
+            self.bullet_speed = 1.5
+            self.alien_speed = 0.5
+        elif self.difficulty_level == 'medium':
+            self.ship_limit = 3
+            self.bullets_allowed = 3
+            self.ship_speed = 1.5
+            self.bullet_speed = 3.0
+            self.alien_speed = 1.0
+        elif self.difficulty_level == 'difficult':
+            self.ship_limit = 2
+            self.bullets_allowed = 3
+            self.ship_speed = 3.0
+            self.bullet_speed = 6.0
+            self.alien_speed = 2.0
+
         # fleet_direction of 1 represents right; -1 represents left
         self.fleet_direction = 1
 
@@ -44,3 +56,11 @@ class Settings:
         self.ship_speed *= self.speedup_scale
         self.bullet_speed *= self.speedup_scale
         self.alien_speed *= self.speedup_scale
+
+    def set_difficulty(self, diff_setting):
+        if diff_setting == 'easy':
+            print('easy')
+        elif diff_setting == 'medium':
+            pass
+        elif diff_setting == 'difficult':
+            pass
