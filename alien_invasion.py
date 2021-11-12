8deadf16ac184e309a8047ca02bac3ac8cde2d99
Mark Matlock <techmatlock@gmail.com>
2021-11-12 11:31:31 -0600

    Fixed also check game active when pressing p.

--- alien_invasion.py
+++ alien_invasion.py
@@ -194,7 +194,7 @@ class AlienInvasion:
             sys.exit()
         elif event.key == pygame.K_SPACE:
             self._fire_bullet()
-        elif event.key == pygame.K_p:
+        elif event.key == pygame.K_p and not self.stats.game_active:
             self._start_game()
 
     def _check_keyup_events(self, event):
